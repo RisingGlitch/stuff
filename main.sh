8cfad7a82cf5d5789f638569cb80cf36d4aa1354
Systemless <95631896+RisingGlitch@users.noreply.github.com>
2023-11-23 19:32:10 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,14 +9,14 @@ function installAll() {
   echo "$(tput setaf 6)Installing packages, please wait..."
   install-pkg "http://archive.ubuntu.com/ubuntu/pool/main/g/glib2.0/libglib2.0-0_2.56.4-0ubuntu0.18.04.8_amd64.deb" &> /dev/null
   echo "$(tput setaf 2)Installed dependencies."
-  echo "$(tput setaf 6)Cloning the latest sniper code..."
+  echo "$(tput setaf 6)Cloning the latest stuff code..."
   git reset --hard HEAD
   git pull
-  echo "$(tput setaf 2)Cloned latest version of the sniper."
-  echo "$(tput setaf 6)Installing sniper dependencies..."
+  echo "$(tput setaf 2)Cloned latest version of the stuff."
+  echo "$(tput setaf 6)Installing stuff dependencies..."
   npm install &> /dev/null
-  echo "$(tput setaf 2)Installed sniper dependencies."
-  echo "$(tput setaf 2)Running sniper..."
+  echo "$(tput setaf 2)Installed stuff dependencies."
+  echo "$(tput setaf 2)Running stuff..."
   npx node ./src/index.js
 }
 
